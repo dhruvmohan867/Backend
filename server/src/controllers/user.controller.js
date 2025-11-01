6ae6e02edbea2aefe18d743027ec70aaa047cb1c
Dhruv Mohan Shukla <127665916+dhruvmohan867@users.noreply.github.com>
2025-11-02 00:15:24 +0530

    Update user routes to include change password and current user endpoints

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -426,7 +426,8 @@ const getWatchHistory = asyncHandler(async(req, res) => {
     const user = await User.aggregate([
         {
             $match: {
-                _id: new mongoose.Types.ObjectId(req.user._id)
+                _id: new mongoose.Types.ObjectId.createFromHexString(req.user._id)
+
             }
         },
         {
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, logoutUser, registerUser , refreshAccessToken} from "../controllers/user.controller.js"
+import {loginUser, logoutUser, registerUser , refreshAccessToken, changeCurrentPassword, getCurrentUser} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"  
 import { verifyJwt } from "../middlewares/auth.middleware.js";  
 const router = Router()
@@ -12,4 +12,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJwt , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt , changeCurrentPassword)
+router.route("/current-user").get(verifyJwt , getCurrentUser)
+
 export default router
